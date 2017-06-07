43bf085f760a5d01123cbaabb5e29cb39cde1ba6
Pedro Ribeiro <ribasss@gmail.com>
2017-06-07 16:46:12 +0100

    [AAO-61] - Changed the way the version code is shown in settings.

--- app/src/main/java/pt/aptoide/backupapps/Settings.java
+++ app/src/main/java/pt/aptoide/backupapps/Settings.java
@@ -7,9 +7,7 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceManager;
-
 import com.actionbarsherlock.view.MenuItem;
-
 import pt.aptoide.backupapps.download.event.BusProvider;
 import pt.aptoide.backupapps.util.Constants;
 
@@ -22,63 +20,70 @@ import pt.aptoide.backupapps.util.Constants;
  */
 public class Settings extends BaseSherlockPreferenceActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    addPreferencesFromResource(R.xml.preferences);
 
-        if(Build.VERSION.SDK_INT>=11){
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
-
-        SharedPreferences sPref = PreferenceManager.getDefaultSharedPreferences(this);
-
-        if (sPref.contains(Constants.LOGIN_USER_LOGIN)) {
-            Preference preference = new Preference(this);
-            preference.setOrder(-1);
-            preference.setSelectable(false);
-            preference.setTitle(getString(R.string.settings_logged_text) + sPref.getString(Constants.LOGIN_USER_LOGIN, ""));
-            ((PreferenceCategory)findPreference("login_cat")).addPreference(preference);
-
-                    findPreference("set_server_login").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                        @Override
-                        public boolean onPreferenceClick(Preference preference) {
-                            new Logout(Settings.this).execute();
-                            return false;
-                        }
-                    });
-        } else {
-            findPreference("set_server_login").setTitle(R.string.settings_login_text);
-            findPreference("set_server_login").setSummary(R.string.settings_login_description_text);
-            findPreference("set_server_login").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    BusProvider.getInstance().post(new LoginMoveEvent());
-                    finish();
-                    return false;
-                }
-            });
-        }
-      try {
-        findPreference("version_name").setSummary(String.format(getString(R.string.settings_versioname_text), getPackageManager().getPackageInfo(getPackageName(), 0).versionName));
-      } catch (PackageManager.NameNotFoundException e) {
-        e.printStackTrace();
-      }
+    if (Build.VERSION.SDK_INT >= 11) {
+      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    @Override
-    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+    SharedPreferences sPref = PreferenceManager.getDefaultSharedPreferences(this);
 
-        switch (item.getItemId()){
+    if (sPref.contains(Constants.LOGIN_USER_LOGIN)) {
+      Preference preference = new Preference(this);
+      preference.setOrder(-1);
+      preference.setSelectable(false);
+      preference.setTitle(
+          getString(R.string.settings_logged_text) + sPref.getString(Constants.LOGIN_USER_LOGIN,
+              ""));
+      ((PreferenceCategory) findPreference("login_cat")).addPreference(preference);
 
-            case R.id.abs__home:
-                finish();
-                break;
-            case android.R.id.home:
-                finish();
-                break;
-        }
-
-        return super.onMenuItemSelected(featureId, item);
+      findPreference("set_server_login").setOnPreferenceClickListener(
+          new Preference.OnPreferenceClickListener() {
+            @Override public boolean onPreferenceClick(Preference preference) {
+              new Logout(Settings.this).execute();
+              return false;
+            }
+          });
+    } else {
+      findPreference("set_server_login").setTitle(R.string.settings_login_text);
+      findPreference("set_server_login").setSummary(R.string.settings_login_description_text);
+      findPreference("set_server_login").setOnPreferenceClickListener(
+          new Preference.OnPreferenceClickListener() {
+            @Override public boolean onPreferenceClick(Preference preference) {
+              BusProvider.getInstance()
+                  .post(new LoginMoveEvent());
+              finish();
+              return false;
+            }
+          });
     }
+    try {
+      findPreference("version_name").setTitle(
+          String.format(getString(R.string.settings_version_text),
+              getPackageManager().getPackageInfo(getPackageName(), 0).versionName)
+              + " "
+              + "("
+              + getPackageManager().getPackageInfo(getPackageName(), 0).versionCode
+              + ")");
+    } catch (PackageManager.NameNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override public boolean onMenuItemSelected(int featureId, MenuItem item) {
+
+    switch (item.getItemId()) {
+
+      case R.id.abs__home:
+        finish();
+        break;
+      case android.R.id.home:
+        finish();
+        break;
+    }
+
+    return super.onMenuItemSelected(featureId, item);
+  }
 }
